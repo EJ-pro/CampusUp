cf032b39448215af34bbf615f3cc1cc4b41373ba
EJ-pro <hsshss2522@naver.com>
2024-06-26 17:03:35 +0900

    불필요한 파일 삭제

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // 텍스트 작성과 삭제 즉시 실행 함수
 (function(){
   const spanEl = document.querySelector("main h2 span");
-  const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
+  const txtArr = ['Campus-Up','1:1 멘토링 매칭', '비즈니스 계획서 작성', '투자 유치 전략 개발', '마케팅 전략'];
   let index = 0;
   let currentTxt = txtArr[index].split("");
   function writeTxt(){
